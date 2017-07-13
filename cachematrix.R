cbb6ff3627e35cd27fa31d1ff540cbb066d416c1
xinablak <luisrosaxavier@gmail.com>
2017-07-13 17:58:36 +0100

    Update cachematrix.R
    Had some comments cleaned up and removed unnecessary garbage...

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,6 @@
 ## function: makeCacheMAatrix
 ## this function creates a matrix to cache the inverse
 
-##  function: cacheSolve 
-## uses the chache to solve matrices
-
 makeCacheMatrix <- function(x = matrix()) {
     m<-NULL
     set <- function(y) {
@@ -19,7 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##  function: cacheSolve 
+## uses the chache to solve matrices
 
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
@@ -34,64 +32,3 @@ cacheSolve <- function(x, ...) {
 }
 
 
-### Introduction
-
-#  This second programming assignment will require you to write an R
-#  function that is able to cache potentially time-consuming computations.
-#  For example, taking the mean of a numeric vector is typically a fast
-#  operation. However, for a very long vector, it may take too long to
-#  compute the mean, especially if it has to be computed repeatedly (e.g.
-#                                                                    in a loop). If the contents of a vector are not changing, it may make
-#  sense to cache the value of the mean so that when we need it again, it
-#  can be looked up in the cache rather than recomputed. In this
-#  Programming Assignment you will take advantage of the scoping rules of
-#  the R language and how they can be manipulated to preserve state inside
-#  of an R object.
-#  
-#  ### Example: Caching the Mean of a Vector
-#  
-#  In this example we introduce the `<<-` operator which can be used to
-#  assign a value to an object in an environment that is different from the
-#  current environment. Below are two functions that are used to create a
-#  special object that stores a numeric vector and caches its mean.
-#  
-#  The first function, `makeVector` creates a special "vector", which is
-#  really a list containing a function to
-#  
-#  1.  set the value of the vector
-#  2.  get the value of the vector
-#  3.  set the value of the mean
-#  4.  get the value of the mean
-#
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-# The following function calculates the mean of the special "vector"
-# created with the above function. However, it first checks to see if the
-# mean has already been calculated. If so, it `get`s the mean from the
-# cache and skips the computation. Otherwise, it calculates the mean of
-# the data and sets the value of the mean in the cache via the `setmean`
-# function.
-
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
-}
